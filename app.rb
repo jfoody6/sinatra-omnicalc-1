560714e2deb68af758caf6579e35afc6ef7d8832
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-04-14 15:23:55 +0000

    sqaures

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:layout)
+end 
+
+get("/square/new") do
+  erb(:new_square_calc)
 end
+
+get("/square/results") do
+@user_num = params.fetch("number").to_f
+@sq_result = @user_num ** 2
+
+  erb(:results_square)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root)
+end
+
+get("/square_root/results") do
+
+  @user_num = params.fetch("number").to_f
+  @sq_root_result = @user_num ** 0.5
+  
+    erb(:results_square_root)
+  
+  end
